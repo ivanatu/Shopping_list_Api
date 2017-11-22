504b8aa0871fde275a3034dfceb60037cfec26f7
ivanatu <ivanatu@gmail.com>
2017-11-22 16:44:01 +0300

    updated the config for testing

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,19 @@ import os
 from app.models import db
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+TEST = True
 
 POSTGRES = {
-    'user': 'postgres',
+    'user': 'ivan2',
     'pw': '1234',
     'db': 'shopping_list',
+    'host': 'localhost',
+    'port': '5432',
+}
+TEST_POSTGRES = {
+    'user': 'postgres',
+    'pw': '',
+    'db': 'tests',
     'host': 'localhost',
     'port': '5432',
 }
@@ -16,12 +24,11 @@ app.config['DEBUG'] = True
 if os.environ.get('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-# %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-# app.config['SQLALCHEMY_DATABASE_URI'] =
+    if TEST:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % TEST_POSTGRES
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
-"postgresql://ivan:1234@localhost/shopping_list"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'this-is-my-secret-key'
 db.init_app(app)
