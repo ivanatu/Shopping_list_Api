5d2c7b386ec7b79dc91d63f262e96783dc9277e4
baron <baron@barons-MacBook-Pro.local>
2017-12-07 11:27:27 +0300

    Change Test to true

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_DATABASE_URI2, SECRET_KEY
+#from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_DATABASE_URI2, SECRET_KEY
 
 shop_api = Flask(__name__, template_folder='./templates', static_folder='./static')
 TEST = True
@@ -12,9 +12,9 @@ if os.environ.get('DATABASE_URL'):
     shop_api.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 else:
     if TEST:
-        shop_api.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+        shop_api.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI2']
     else:
-        shop_api.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI2
+        shop_api.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
 
 shop_api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 shop_api.config['SECRET_KEY'] = os.environ['SECRET_KEY']
