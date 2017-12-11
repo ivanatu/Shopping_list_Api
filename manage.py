77e81364ddea53295cf3a5d5f3f2b24cddc15c76
baron <baron@barons-MacBook-Pro.local>
2017-12-11 19:08:24 +0300

    Modify name to cater for change in init file

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app, db
+from app import shop_api, db
 
-manager = Manager(app)
-migrate = Migrate(app, db)
+manager = Manager(shop_api)
+migrate = Migrate(shop_api, db)
 
 manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
