8d6e971f1f9f1d82ad8e6129687dfdd2934fe1f6
baron <baron@barons-MacBook-Pro.local>
2017-12-16 14:35:04 +0300

    Modify filtering for shopping list

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify, render_template, session, \
-    make_response
+	make_response
 from app.models import Item, Shopping_list, User, db
 from app import shop_api
 from werkzeug.security import generate_password_hash, \
-    check_password_hash
+	check_password_hash
 from app.handlers import validate, required, validate_json
 
 
@@ -31,13 +31,13 @@ def register():
 
 	if len(require) != 0:
 		return make_response(jsonify({'status': 'fail',
-                                      'message': require
-                                      }), 400)
+									  'message': require
+									  }), 400)
 
 	invalid = validate(data)
 	if invalid:
 		return make_response(jsonify({'status': 'fail',
-                                      'message': invalid}), 400)
+									  'message': invalid}), 400)
 
 	user = User.query.filter_by(email=data['email']).first()
 	if user is None:
@@ -51,11 +51,11 @@ def register():
 		return jsonify({'email': user.email,
 						'status': 'pass',
 						'message': 'user account created successfully'
-                        }), 201
+						}), 201
 
 	return jsonify({'status': 'fail',
-                    'message': 'user email already exists'
-                    }), 200
+					'message': 'user email already exists'
+					}), 200
 
 
 @shop_api.route('/auth/login', methods=['POST'])
@@ -66,14 +66,14 @@ def login():
 	"""
 	if not request.json:
 		return jsonify({'status': 'fail',
-                        'message': 'form errors'
-                        }), 400
+						'message': 'form errors'
+						}), 400
 	data = request.json
 	invalid = validate(data)
 	if invalid:
 		return make_response(jsonify({'status': 'fail',
-                                      'message': invalid
-                                      }), 400)
+									  'message': invalid
+									  }), 400)
 	user = User.query.filter_by(email=data['email']).first()
 	# log message and authenticate user
 	if user is not None:
@@ -87,8 +87,8 @@ def login():
 								'message': 'login was successful'}), 201
 		return jsonify({'status': 'fail',
 						'message': 'wrong password or email or '
-                                   'may be user does\'t exist'
-                        }), 200
+								   'may be user does\'t exist'
+						}), 200
 	return jsonify({"error": "user not found. please register"}), 401
 
 
@@ -101,9 +101,10 @@ def reset_password():
 	"""
 	if not request.json:
 		return jsonify({'status': 'fail',
-                        'message': 'form errors'
-                        }), 400
+						'message': 'form errors'
+						}), 400
 	data = request.json
+
 	auth_header = request.headers.get('Authorization')
 	if auth_header:
 		user_id = User.decode_token(auth_header)
@@ -111,31 +112,31 @@ def reset_password():
 			invalid = validate(data)
 			if invalid:
 				return make_response(jsonify({'status': 'fail',
-                                              'message': invalid
-                                              }), 400)
+											  'message': invalid
+											  }), 400)
 			# locate user and check the old password
 			user = User.query.filter_by(email=data['email']).first()
 			if user and check_password_hash(user.password,
-                                            data['old_password']):
+											data['old_password']):
 				user.password = generate_password_hash(data['new_password'])
 				db.session.commit()
 				return jsonify({'email': user.email,
 								'status': 'pass',
 								'message': 'password was changed successfully'
-                                }), 201
+								}), 201
 
 			return jsonify({'status': 'fail',
 							'message': 'wrong email or password or '
-                                       'may be user does\'t exist'
-                            }), 200
+									   'may be user does\'t exist'
+							}), 200
 
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists', methods=['POST'])
@@ -145,8 +146,8 @@ def add_a_list():
 	"""
 	if not request.json:
 		return jsonify({'status': 'fail',
-                        'message': 'form errors'
-                        }), 400
+						'message': 'form errors'
+						}), 400
 	data = request.json
 	auth_header = request.headers.get('Authorization')
 	if auth_header:
@@ -155,17 +156,17 @@ def add_a_list():
 			invalid = validate(data)
 			if invalid:
 				return make_response(jsonify({'status': 'fail',
-                                              'message': invalid
-                                              }), 400)
+											  'message': invalid
+											  }), 400)
 			# create a list
 			a_list = Shopping_list.query.filter_by(
-                list=data['list'],
-                user_id=user_id).first()
+				list=data['list'],
+				user_id=user_id).first()
 
 			if a_list is None:
 				the_list = Shopping_list(
-                    list=data['list'],
-                    user_id=user_id)
+					list=data['list'],
+					user_id=user_id)
 				db.session.add(the_list)
 				db.session.commit()
 				response = jsonify({'id': the_list.id,
@@ -175,14 +176,14 @@ def add_a_list():
 									})
 				return response, 201
 			return jsonify({'status': 'fail',
-                            'message': 'list already exists'
-                            }), 404
+							'message': 'list already exists'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists', methods=['GET'])
@@ -201,24 +202,24 @@ def view_all_lists():
 		if not isinstance(user_id, str):
 			results = []
 			# query parameters
-            # this parameter contains the name of the list
+			# this parameter contains the name of the list
 			q = request.args.get('q', None)
-            # limits the number of records to 50 per page
+			# limits the number of records to 50 per page
 			limit = request.args.get('limit', 50,
-                                     type=int)
-            # page one is default,
-            # but page can be passed as an argument
+									 type=int)
+			# page one is default,
+			# but page can be passed as an argument
 			page = request.args.get('page', 1,
-                                    type=int)
+									type=int)
 
 			if q is not None:
 				lists = Shopping_list.query.filter(
-                    Shopping_list.list.like("%" + q.strip() + "%")). \
+					Shopping_list.list.like("%" + q.strip() + "%")). \
 					filter_by(user_id=user_id).paginate(
-                    page, limit, False).items
+					page, limit, False).items
 			else:
 				lists = Shopping_list.query.filter_by(
-                    user_id=user_id).paginate(page, limit, False).items
+					user_id=user_id).paginate(page, limit, False).items
 			for a_list in lists:
 				result = {
 					'id': a_list.id,
@@ -230,17 +231,17 @@ def view_all_lists():
 								'count': str(len(results)),
 								'status': 'pass',
 								'message': 'lists found'
-                                }), 200
+								}), 200
 			return jsonify({'count': '0',
-                            'status': 'fail',
-                            'message': 'no lists found'
-                            }), 404
+							'status': 'fail',
+							'message': 'no lists found'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:id>', methods=['GET'])
@@ -254,7 +255,7 @@ def get_a_list(id):
 		user_id = User.decode_token(auth_header)
 		if not isinstance(user_id, str):
 			a_list = Shopping_list.query.filter_by(
-                id=id, user_id=user_id).first()
+				id=id, user_id=user_id).first()
 			if a_list is not None:
 				response = jsonify({'list': dict(id=a_list.id,
 												 list=a_list.list
@@ -264,15 +265,15 @@ def get_a_list(id):
 									'message': 'list found'})
 				return response, 200
 			return jsonify({'count': '0',
-                            'status': 'fail',
-                            'message': 'list not found'
-                            }), 404
+							'status': 'fail',
+							'message': 'list not found'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:id>', methods=['PUT'])
@@ -290,10 +291,10 @@ def update_a_list(id):
 			invalid = validate(data)
 			if invalid:
 				return make_response(jsonify({'status': 'fail',
-                                              'message': invalid
-                                              }), 400)
+											  'message': invalid
+											  }), 400)
 			the_list = Shopping_list.query.filter_by(
-                id=id, user_id=user_id).first()
+				id=id, user_id=user_id).first()
 			if the_list is not None:
 				the_list.list = data['list']
 				db.session.commit()
@@ -304,14 +305,14 @@ def update_a_list(id):
 									'message': 'list updated'})
 				return response, 201
 			return jsonify({'status': 'fail',
-                            'message': 'list doesnot exist'
-                            }), 400
+							'message': 'list doesnot exist'
+							}), 400
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:id>', methods=['DELETE'])
@@ -324,22 +325,22 @@ def delete_a_list(id):
 	if auth_header:
 		user_id = User.decode_token(auth_header)
 		if not isinstance(user_id, str):
-			the_list = Shopping_list.query.filter_by(id=id).first()
+			the_list = Shopping_list.query.filter_by(id=id, user_id=user_id).first()
 			if the_list is not None:
 				db.session.delete(the_list)
 				db.session.commit()
 				return jsonify({'status': 'pass',
-                                'message': 'list deleted'
-                                }), 200
+								'message': 'list deleted'
+								}), 200
 			return jsonify({'status': 'fail',
-                            'message': 'list not deleted'
-                            }), 404
+							'message': 'list not deleted'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 # -------------------------------------------------------------------------------------------------
@@ -355,31 +356,36 @@ def add_items_list(id):
 	if auth_header:
 		user_id = User.decode_token(auth_header)
 		if not isinstance(user_id, str):
-			the_list = Shopping_list.query.filter_by(
-                id=id).first()
+			invalid = validate(data)
+			if invalid:
+				return make_response(jsonify({'status': 'fail',
+											  'message': invalid
+											  }), 400)
+				the_list = Shopping_list.query.filter_by(
+					id=id, user_id=user_id).first()
 			if the_list is not None:
 				# if 'name' in data and 'price' in data:
 				# add an item to the list
 				item = Item(name=data['name'],
-                            price=data['price'],
-                            List_id=id)
+							price=data['price'],
+							List_id=id)
 				db.session.add(item)
 				db.session.commit()
 				return jsonify({'item_id': item.id,
-                                'name': item.name,
+								'name': item.name,
 								'price': item.price,
 								'status': 'pass',
-                                'message': 'item added to list'
-                                }), 201
+								'message': 'item added to list'
+								}), 201
 			return jsonify({'status': 'fail',
-                            'message': 'list does not exist'
-                            }), 404
+							'message': 'list does not exist'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:id>/items', methods=['GET'])
@@ -390,25 +396,25 @@ def get_items_list(id):
 		if not isinstance(user_id, str):
 			results = []
 			# query parameters
-            # this parameter contains the name of the list
+			# this parameter contains the name of the list
 			q = request.args.get('q', None)
-            # limits the number of records to 50 per page (optional)
+			# limits the number of records to 50 per page (optional)
 			limit = request.args.get('limit', 50,
-                                     type=int)
-            # page one is default, but page can be passed as an argument (optional)
+									 type=int)
+			# page one is default, but page can be passed as an argument (optional)
 			page = request.args.get('page', 1,
 									type=int)
 			a_list = Shopping_list.query.filter_by(
-                id=id).first()
+				id=id, user_id=user_id).first()
 			if a_list is not None:
 				if q is not None:
 					items = Item.query.filter(
-                        Item.name.like("%" + q.strip() + "%")). \
+						Item.name.like("%" + q.strip() + "%")). \
 						filter_by(List_id=id).paginate(
-                        page, limit, False).items
+						page, limit, False).items
 				else:
 					items = Item.query.filter_by(
-                        List_id=id).paginate(page, limit, False).items
+						List_id=id).paginate(page, limit, False).items
 				for a_items in items:
 					result = {
 						'itemid': a_items.id,
@@ -422,22 +428,22 @@ def get_items_list(id):
 									'status': 'pass',
 									'message': 'lists found'}), 200
 				return jsonify({'count': '0',
-                                'status': 'fail',
-                                'message': 'no items found'
-                                }), 404
+								'status': 'fail',
+								'message': 'no items found'
+								}), 404
 			return jsonify({'status': 'fail',
-                            'message': 'list not found'
-                            }), 404
+							'message': 'list not found'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:list_id>/items/<int:item_id>',
-                methods=['PUT'])
+				methods=['PUT'])
 def update_list_item(list_id, item_id):
 	"""
 	This endpoint will update a given item on a given list
@@ -448,10 +454,10 @@ def update_list_item(list_id, item_id):
 		user_id = User.decode_token(auth_header)
 		if not isinstance(user_id, str):
 			the_list = Shopping_list.query.filter_by(
-                id=list_id).first()
+				id=list_id, user_id=user_id).first()
 			if the_list is not None:
 				the_item = Item.query.filter_by(
-                    id=item_id).first()
+					id=item_id).first()
 				if the_item is not None:
 					the_item.name = data['name']
 					the_item.price = data['price']
@@ -461,23 +467,23 @@ def update_list_item(list_id, item_id):
 												 price=the_item.price),
 									'status': 'pass',
 									'message': 'item updated'
-                                    }), 201
+									}), 201
 				return jsonify({'status': 'fail',
-                                'message': 'item not updated'
-                                }), 400
+								'message': 'item not updated'
+								}), 400
 			return jsonify({'status': 'fail',
-                            'message': 'list does not exist'
-                            }), 404
+							'message': 'list does not exist'
+							}), 404
 		return jsonify({'status': 'fail',
-                        "message": "Error with your login, please try again"
-                        }), 401
+						"message": "Error with your login, please try again"
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
 
 
 @shop_api.route('/shoppinglists/<int:list_id>/items/<int:item_id>',
-                methods=['DELETE'])
+				methods=['DELETE'])
 def delete_item_from_list(list_id, item_id):
 	"""
 	This endpoint will delete an item on given list
@@ -486,7 +492,7 @@ def delete_item_from_list(list_id, item_id):
 	if auth_header:
 		user_id = User.decode_token(auth_header)
 		if not isinstance(user_id, str):
-			the_list = Shopping_list.query.filter_by(id=list_id).first()
+			the_list = Shopping_list.query.filter_by(id=list_id, user_id=user_id).first()
 			if the_list is not None:
 				the_item = Item.query.filter_by(id=item_id).first()
 				if the_item is not None:
@@ -494,17 +500,17 @@ def delete_item_from_list(list_id, item_id):
 					db.session.delete(the_item)
 					db.session.commit()
 					return jsonify({'status': 'pass',
-                                    'message': 'item deleted'
-                                    }), 200
+									'message': 'item deleted'
+									}), 200
 				return jsonify({'status': 'fail',
-                                'message': 'item not found'
-                                }), 404
+								'message': 'item not found'
+								}), 404
 			return jsonify({'status': 'fail',
-                            'message': 'list does not exist'
-                            }), 404
+							'message': 'list does not exist'
+							}), 404
 		return jsonify({'status': 'fail', "message": "Error with your login,"
 													 " please try again"
-                        }), 401
+						}), 401
 	return jsonify({'status': 'fail',
-                    "message": " cant access to login"
-                    }), 401
+					"message": " cant access to login"
+					}), 401
