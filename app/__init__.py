fea37404734fc8a12896758e3e406e0ea4eaa213
baron <baron@barons-MacBook-Pro.local>
2018-02-04 19:43:05 +0300

    Add CORS to init file to enable other apps access the API

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 shop_api = Flask(__name__, template_folder='./templates', static_folder='./static')
-TEST = True
+CORS(shop_api)
+TEST = False
 
 shop_api.config['DEBUG'] = True
 if os.environ.get('DATABASE_URL'):
@@ -18,5 +20,8 @@ shop_api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 shop_api.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 db = SQLAlchemy(shop_api)
 db.init_app(shop_api)
+shop_api.config['CORS_HEADERS'] = 'Content-Type'
+
+
 
 from app import views
